cad9e767881a144bcd28d805e7c25d3544be2f00
Aman Kumar <akumar10350@truechip.net>
2023-10-13 18:15:40 +0530

    spring initialized

---
+++ src/main/java/com/springbootdhan/SpringBootDhanApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDhanApplication.class, args);
+	}
+
+}
